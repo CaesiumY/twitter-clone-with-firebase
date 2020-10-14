8fde5a66e1286a5df1921536cdae0a1f4f56195e
caesiumy <mn0658@naver.com>
2020-10-14 22:18:02 +0900

    feat: Add auth form only frames

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          name="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+    </div>
+  );
 };
 
 export default Auth;
