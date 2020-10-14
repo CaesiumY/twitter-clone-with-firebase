a7a68ee5cc97ad981cdbc0663d2c3620ab61400e
caesiumy <mn0658@naver.com>
2020-10-14 23:16:32 +0900

    feat: Add login redirect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "route";
 import { authService } from "firebaseConfig";
 
 function App() {
-  const [isAuth, setIsAuth] = useState(authService.currentUser);
-  console.log("App -> isAuth", isAuth);
+  const [isInit, setIsinit] = useState(false);
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      user ? setIsAuth(true) : setIsAuth(false);
+
+      setIsinit(true);
+    });
+  }, []);
+
   return (
     <>
-      <AppRouter isAuth={isAuth}></AppRouter>
+      {isInit ? <AppRouter isAuth={isAuth}></AppRouter> : "Initializing..."}
       <footer>&copy; {new Date().getFullYear()} FireTwit</footer>
     </>
   );
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -5,6 +5,7 @@ const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -21,11 +22,13 @@ const Auth = () => {
         data = await authService.signInWithEmailAndPassword(email, password);
       }
 
-      console.log(data);
+      console.log("onSubmit -> data", data);
     } catch (error) {
-      console.error(error);
+      setError(error.message);
     }
   };
+
+  const toggleNewAccount = () => setNewAccount((prev) => !prev);
 
   return (
     <div>
@@ -44,7 +47,16 @@ const Auth = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <input type="submit" value="Log In" />
+        <input type="submit" value={newAccount ? "Sign In" : "Log In"} />
+        <input
+          type="checkbox"
+          name="isNewAccount"
+          id="isNewAccount"
+          value="isNewAccount"
+          onChange={toggleNewAccount}
+        />
+        <label htmlFor="isNewAccount">Already have Account?</label>
+        {error}
       </form>
       <div>
         <button>Continue with Google</button>
