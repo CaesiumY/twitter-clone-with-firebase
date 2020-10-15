6e01b73e726353fa24d0b6aceef8116e3ab8f5f0
caesiumy <mn0658@naver.com>
2020-10-15 22:20:08 +0900

    feat: Add firestore add & get

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { dbService } from "../firebaseConfig";
 
 const Home = () => {
   const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+
+  const getTweets = async () => {
+    const tweetData = await dbService.collection("tweets").get();
+
+    tweetData.forEach((tweetItem) => {
+      const tweetObj = {
+        id: tweetItem.id,
+        ...tweetItem.data(),
+      };
+
+      setTweets((prev) => [tweetObj, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getTweets();
+  }, []);
 
   const onChange = (e) => {
     const { value } = e.target;
@@ -9,10 +28,15 @@ const Home = () => {
     setTweet(value);
   };
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
 
-    console.log("tweet:", tweet);
+    await dbService.collection("tweets").add({
+      tweet,
+      createdAt: Date.now(),
+    });
+
+    setTweet("");
   };
 
   return (
@@ -26,6 +50,12 @@ const Home = () => {
         />
         <input type="submit" value="Tweet" />
       </form>
+
+      {tweets.map((item) => (
+        <div key={item.id}>
+          <h4>{item.tweet}</h4>
+        </div>
+      ))}
     </>
   );
 };
