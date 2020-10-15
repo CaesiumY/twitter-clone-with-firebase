b8e90b9f05d783682eabce077be65709e68f58b8
caesiumy <mn0658@naver.com>
2020-10-15 21:40:40 +0900

    feat: Add home tweet submit form

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,6 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -16,3 +17,5 @@ firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 
 export const authService = firebase.auth();
+
+export const database = firebase.database();
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [tweet, setTweet] = useState("");
+
+  const onChange = (e) => {
+    const { value } = e.target;
+
+    setTweet(value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("tweet:", tweet);
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="What's on your mind?"
+          value={tweet}
+          onChange={onChange}
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+    </>
+  );
 };
 
 export default Home;
