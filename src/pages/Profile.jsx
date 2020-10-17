b205125bf41f0c2b0476905af264c5568bf76028
caesiumy <mn0658@naver.com>
2020-10-17 21:24:45 +0900

    feat: Add updating user displayName in Profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { authService, dbService } from "firebaseConfig";
 
 const Profile = ({ userObj }) => {
+  const [newDisplayName, setNewDisplayName] = useState(
+    userObj.displayName || userObj.email.split("@")[0]
+  );
   const history = useHistory();
 
   const onSignOut = async () => {
@@ -17,16 +20,35 @@ const Profile = ({ userObj }) => {
       .orderBy("createdAt")
       .get();
 
-    console.log(myTweets.docs.map((doc) => console.log("docs", doc.data())));
+    console.log(myTweets.docs.map((doc) => doc.data()));
   };
 
   useEffect(() => {
     getMyTweets();
-  });
+  }, []);
+
+  const onChange = (e) => {
+    const { value } = e.target;
+    setNewDisplayName(value);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+  };
 
   return (
     <>
       <p>Profile</p>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={newDisplayName} onChange={onChange} />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onSignOut}>Log Out</button>
     </>
   );
