f8ae63c5ddc8fd142648d24020f7b46efe29f7ac
caesiumy <mn0658@naver.com>
2020-10-20 00:50:38 +0900

    fix: Remove small warning

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -10,14 +10,10 @@ const AuthForm = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     try {
-      let data;
       if (newAccount) {
-        data = await authService.createUserWithEmailAndPassword(
-          email,
-          password
-        );
+        await authService.createUserWithEmailAndPassword(email, password);
       } else {
-        data = await authService.signInWithEmailAndPassword(email, password);
+        await authService.signInWithEmailAndPassword(email, password);
       }
     } catch (error) {
       setError(error.message);
